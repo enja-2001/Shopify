f474d28e296a9bc5cd7f7b1fa9df8600a6b40f68
Md. Enjamum Hossain <62800865+enja-2001@users.noreply.github.com>
2021-01-26 10:06:30 +0530

    Update HomeActivity.java

--- app/src/main/java/com/example/shopify/HomeActivity.java
+++ app/src/main/java/com/example/shopify/HomeActivity.java
@@ -123,30 +123,21 @@ public class HomeActivity extends AppCompatActivity  implements NavigationView.O
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 switch (item.getItemId()) {
+                        
                     case R.id.item1:
                         itemView.removeView(notificationBadge);
-
-
                         editor.putString("notificationBadgeCount", Integer.toString(0));
                         editor.apply();
-
                         getSupportFragmentManager().beginTransaction().replace(R.id.myframe, new NotificationFragment()).commit();
                         break;
 
-
-//                    case R.id.item2:
-//                        getSupportFragmentManager().beginTransaction().replace(R.id.myframe, new FavouritesFragment()).commit();
-//                        break;
                     case R.id.item3:
                         getSupportFragmentManager().beginTransaction().replace(R.id.myframe, new HomeFragment()).commit();
                         break;
+                        
                     case R.id.item10:
                         getSupportFragmentManager().beginTransaction().replace(R.id.myframe, new OrdersFragment()).commit();
                         break;
-
-//                    case R.id.item13:
-//                        getSupportFragmentManager().beginTransaction().replace(R.id.myframe, new WishlistFragment()).commit();
-//                        break;
                 }
                 return true;
             }
